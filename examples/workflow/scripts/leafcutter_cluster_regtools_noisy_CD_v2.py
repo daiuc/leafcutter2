9f4ec0ce81fb06b10b2773ad0fec9e577e236948
daiuc <chaodai@uchicago.edu>
2023-03-30 18:07:39 -0500

    now only use annotation to determine func or noisy

--- examples/workflow/scripts/leafcutter_cluster_regtools_noisy_CD_v2.py
+++ examples/workflow/scripts/leafcutter_cluster_regtools_noisy_CD_v2.py
@@ -1156,18 +1156,18 @@ def annotate_noisy(options):
         intronid = (chrom,int(s),int(e),strand)
         usages = [int(x.split("/")[0]) / (float(x.split("/")[1])+0.1) for x in ln[1:]] # intron usage ratios
         if sum(usages) == 0: continue
-        med = median(usages) # median usage ratio among samples
+        # med = median(usages) # median usage ratio among samples
 
-        if med >= 0.1:
-            # If median usage is above 0.1, consider it functional
-            classification = "putative_functional" # aka 'F'
+        # if med >= 0.1:
+        #     # If median usage is above 0.1, consider it functional
+        #     classification = "putative_functional" # aka 'F'
+        # else:
+        if intronid in dic_noise:
+            # intron found in GTEx/annotation, so use its classification, i.e. 'putative_functional' # aka 'F'
+            classification = dic_noise[intronid] # dic_noise: { k=(chr, start, end, strand) : v=classfication }
         else:
-            if intronid in dic_noise:
-                # intron found in GTEx/annotation, so use its classification, i.e. 'putative_functional' # aka 'F'
-                classification = dic_noise[intronid] # dic_noise: { k=(chr, start, end, strand) : v=classfication }
-            else:
-                # intron not found in GTEx/annotation and med < 0.1
-                classification = "noisy" # aka 'N'
+            # intron not found in GTEx/annotation and med < 0.1
+            classification = "noisy" # aka 'N'
             
         # if classification not in dic_usage: # dic_usage { k=classification : v=[] }
         #     dic_usage[classification] = [] # initialize dic_usage, dict to store introns by class
